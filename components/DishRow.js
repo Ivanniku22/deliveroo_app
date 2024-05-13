dbbbc5adb1e2a13903621fbd12d6ed5d336cb8e0
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2024-05-13 12:37:56 +0530

    Finished initial setup

--- components/DishRow.js
+++ components/DishRow.js
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import Currency from 'react-currency-formatter'
 import { urlFor } from '../sanity'
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid'
 
 const DishRow = ({
     id,
@@ -11,13 +12,15 @@ const DishRow = ({
     image,
 }) => {
 
-    const [isPRessed,setIsPressed] = useState(false)
+    const [isPressed,setIsPressed] = useState(false)
 
 
   return (
     <>
-    <View>
-      <TouchableOpacity className='bg-white border p-4 border-gray-200 '>
+      <TouchableOpacity 
+        onPress={() =>setIsPressed(!isPressed)}
+        className={`bg-white border p-4 border-gray-200 ${isPressed && "border-b-0"}`} 
+        >
         <View className='flex-row '>
             <View className='flex-1 pr-2'>
                 <Text className='text-lg mb-1'>{name}</Text>
@@ -41,7 +44,26 @@ const DishRow = ({
             </View>
         </View>
       </TouchableOpacity>
-    </View>
+
+      {isPressed && (
+        <View className='bg-white px-4'>
+            <View className='flex-row items-center space-x-2 pb-3'>
+                <TouchableOpacity>
+                    <MinusCircleIcon
+                        color={'#00CCBB'} 
+                      size={40}  
+                    />
+                </TouchableOpacity>
+                <Text>0</Text>
+                <TouchableOpacity>
+                    <PlusCircleIcon 
+                        color={'#00CCBB'} 
+                      size={40} 
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+      )}
     </>
   )
 }
